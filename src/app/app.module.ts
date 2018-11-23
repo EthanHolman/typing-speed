b749aa82928b8305c7dd4fa58fb1af4fc2e1ec27
Ethan Holman <96ethanh@gmail.com>
2018-11-22 16:15:30 -0800

    set up basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpeedTestModule } from './modules/speed-test/speed-test.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SpeedTestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
