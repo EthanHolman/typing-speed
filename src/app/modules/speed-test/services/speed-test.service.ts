8fd1e24baf8923739811b7823ef5d93ea765b650
Ethan Holman <96ethanh@gmail.com>
2018-12-09 12:45:04 -0800

    Refactored typing tests into service (eventually will call an API)

--- src/app/modules/speed-test/services/speed-test.service.ts
+++ src/app/modules/speed-test/services/speed-test.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { WordLoaderService } from '../../core/services/word-loader.service';
 import { WordListApiService } from '../../core/services/word-list-api.service';
+import { TypingTestApiService } from '../../core/services/typing-test-api.service';
+import { TypingTestMode } from '../../core/models/typing-test-mode';
 
 @Injectable()
 export class SpeedTestService {
+    typingTests: TypingTestMode[] = [];
     selectedSpeedTestId: number = 1;
     words: string[] = [];
     lines: any[] = [];
 
     constructor(private _wordLoader: WordLoaderService,
-                private _WordListApiService: WordListApiService) { }
+                private _wordListApiService: WordListApiService,
+                private _typingTestApiService: TypingTestApiService) {
+        this.loadTypingTests();
+    }
 
     reset(): void {
+        this.words = [];
+        this.lines = [];
         this.loadWords();
     }
 
@@ -37,6 +45,21 @@ export class SpeedTestService {
         this.lines.push(this.getRandomWordLine());
     }
 
+    loadTest(testId: number): void {
+        this.selectedSpeedTestId = testId;
+        this.reset();
+    }
+
+    private loadTypingTests(): void {
+        this._typingTestApiService.getTypingTests()
+            .subscribe((tests: TypingTestMode[]) => {
+                this.typingTests = tests;
+            }, (error: any) => {
+                console.error('Could not load typing tests');
+                this.typingTests = [];
+            });
+    }
+
     private getRandomNumber(min: number, max: number): number {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -44,7 +67,9 @@ export class SpeedTestService {
     }
 
     private loadWords(): void {
-        this._WordListApiService.getWordList('300mostcommon.txt')
+        const textFileName = this.typingTests.find(x => x.id === this.selectedSpeedTestId).textFileName;
+
+        this._wordListApiService.getWordList(textFileName)
             .subscribe((data: any) => {
                 this.words = data;
                 this.onWordsLoaded();
