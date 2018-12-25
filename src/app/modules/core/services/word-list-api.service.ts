027bf0c22331e759ff4c0db82289663162ae09bd
Ethan Holman <96ethanh@gmail.com>
2018-12-24 21:20:56 -0800

    Updated environment files to include API base path

--- src/app/modules/core/services/word-list-api.service.ts
+++ src/app/modules/core/services/word-list-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class WordListApiService {
@@ -13,7 +14,7 @@ export class WordListApiService {
     }
 
     getWordList(name: string): Observable<string[]> {
-        return this._http.get(`/assets/text-files/${name}`, {responseType: 'text'})
+        return this._http.get(`${environment.apiPath}/assets/text-files/${name}`, {responseType: 'text'})
         .pipe(map((data: string) => {
             return data.split(',');
         }));
